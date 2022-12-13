6440c69e6c05ce9e4b66607287636c24b3f17c62
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-13 18:59:57 -0300

    Variáveis

---
+++ 1-receber-e-trabalhar-com-dados/app.py
@@ -0,0 +1,15 @@
+#Tipos de dados que posso armazenar na memória do computador
+variavel_1 = 25.87
+variavel_2, variavel_3 = 15, 16
+
+variavel_4 = True
+
+variavel_5 = "Feito é melhor que perfeito!"
+
+print(variavel_1, variavel_2, variavel_3, variavel_4, variavel_5)
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
+print(type(variavel_5))
