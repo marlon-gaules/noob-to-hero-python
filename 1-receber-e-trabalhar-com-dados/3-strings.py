8c3dcfa5152e079730b8439091661b8b58912100
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-14 08:05:40 -0300

    strings

---
+++ 1-receber-e-trabalhar-com-dados/3-strings.py
@@ -0,0 +1,13 @@
+print('Codar todos os dias!')
+print("Codar todos os dias!")
+
+print('Codar "todos" os dias!')
+
+print('''Codar todos os dias!
+Isso é muito bom
+Fuiii''')
+
+print("Olá meu nome é \n marlon")
+
+nome = "Marlon"
+print(len(nome))
