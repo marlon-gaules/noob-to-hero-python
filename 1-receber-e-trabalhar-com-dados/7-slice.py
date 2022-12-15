3e15b5e1fb51cedfb4641c5b0663848cb6545736
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-15 08:40:13 -0300

    Arrumando desafio 7-slice.py

--- 1-receber-e-trabalhar-com-dados/7-slice.py
+++ 1-receber-e-trabalhar-com-dados/7-slice.py
@@ -21,7 +21,7 @@ print(link[:-5])
 # DESAFIO
 # 1 - Encontre o índice de 'o' dentro da variável biblioteca
 biblioteca = 'Biblioteca'
-print(biblioteca[5])
+print(biblioteca.index("o"))
 
 # 2 - Usando a frase
 frase = 'Desenvolvimento De Aplicações'
