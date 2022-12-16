708ebba3d9b98704f1046b26a41c0f99353fc620
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-16 08:06:13 -0300

    Adicionando conteúdo sobre Números e Operações Matemáticas

---
+++ 1-receber-e-trabalhar-com-dados/10-numeros-e-operacoes-matematicas.py
@@ -0,0 +1,30 @@
+import math
+a = 20
+b = 28.5
+print(type(a))
+print(type(b))
+
+# operações matematicas
+print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 // 6)
+print(10 % 6)
+print(10 ** 6)
+
+# atalho para atribuição mais rápido
+a = 10
+a = a + 5
+a += 5
+
+b = 20
+b = b - 2
+b -= 2
+
+# arredondamento
+print(round(5.7))
+print(round(5.3))
+print(round(5.7))
+
+print(math.ceil(2.2))
