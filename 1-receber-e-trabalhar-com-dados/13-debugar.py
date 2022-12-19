0d6a086326e744ff9247a7bc676aba04f87152dc
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-19 09:40:17 -0300

    Debugando código

---
+++ 1-receber-e-trabalhar-com-dados/13-debugar.py
@@ -0,0 +1,10 @@
+print("olá")
+
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+
+calcular_preco_combo(30, 20)
+print("Programa finalizado!")
