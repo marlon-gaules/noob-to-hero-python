a0814127f06c5c7909951b59628ba00c6d182a20
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-24 16:58:09 -0300

    Adicionando Lógica de comparação c Operadores lógicos

---
+++ 3-aprendendo-controlar-fluxo-dos-programas/1-logica_de_comparação_c_operadores_logicos.py
@@ -0,0 +1,16 @@
+# comprações numéricas
+idade = 15
+print(idade > 17)
+print(idade < 17)
+print(idade <= 17)
+print(idade >= 17)
+print(idade == 17)
+# totalmente diferente
+print(idade != 17)
+
+# comparações entre outros tipos
+print(True == True)
+print("Rafael" == "Rafael")
+print("Rafael" == "rafael")
+print("f" > "c")  # comparação na posição do alfabeto
+print(5 == "5")
