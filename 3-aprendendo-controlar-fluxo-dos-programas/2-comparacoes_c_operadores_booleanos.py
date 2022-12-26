fe73f04488f5c3b88a515260306ee140aba9bd32
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-26 07:55:26 -0300

    Atualizando 2 - Comparacões c operadores booleanos

--- 3-aprendendo-controlar-fluxo-dos-programas/2-comparacoes_c_operadores_booleanos.py
+++ 3-aprendendo-controlar-fluxo-dos-programas/2-comparacoes_c_operadores_booleanos.py
@@ -1,30 +1,20 @@
-# temos duas informações da pessoa, a idade e o convite.
-# então a condição, é que a pessoa só pode entrar no evento caso seja maior de 21 anos, e tenha um convite
 idade = 21
 possui_convite = False
 filho_do_dono = True
 
-# uma condição, envolvendo duas condicionais (and), atende se as duas condições estiver de acordo
 print(idade >= 21 and possui_convite == True)
 
-# o (or) atende se alguma condição estiver de acordo
 print(idade >= 21 or possui_convite == True)
 
-# envolvendo 3 condições
-# informo que aceito alguma das duas opções abaixo
 print((idade >= 21 and possui_convite == True) or (filho_do_dono == True))
 
-# você só pode trabalhar aqui se for maior idade e possuir carteira de trabalho
 maior_idade = True
 possui_carteira_de_trabalho = False
 possui_veiculo_proprio = True
 
 print(maior_idade == True and possui_carteira_de_trabalho == True)
-# posso imprimir o mesmo resultado acima, diminuindo a linha de código
 print(maior_idade and possui_carteira_de_trabalho)
 
-# negativando uma condição
-# queremos contratar pessoas que ainda não possuem veiculo próprio, mas já possuam uma carteira de trabalho
 print(possui_carteira_de_trabalho == True and not possui_veiculo_proprio)
 
 # Desafio
