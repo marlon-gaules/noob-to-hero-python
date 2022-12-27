d85f51250e29ecf7e304e700ea1276e7abd2f24e
Marlon Costa e Sousa <marloncostaesousa@gmail.com>
2022-12-27 06:30:56 -0300

    Adicionando 4 - convertendo entre tipos

---
+++ 3-aprendendo-controlar-fluxo-dos-programas/4-convertendo_entre_tipos.py
@@ -0,0 +1,18 @@
+# convertendo um valor dentro de parenteses, entendendo o valor que o usuário digitar e comparar
+idade = input("Qual a sua idade? ")
+print(int(idade) > 18)
+
+# a mesma coisa funciona para converter string
+print(type(str(5)))
+
+# recebendo valores decimais
+altura_da_parede = input("Qual a altura da parede? ")
+print(float(altura_da_parede) > 2.10)
+
+# tipos de conversões mais comuns, a ultima é dicionarios
+int()
+str()
+float()
+list()
+tuple()
+dict()
